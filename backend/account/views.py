f03286a0546d94bc3cff32dcd52b1d6a9bede35b
ahmad <ahmad@kali.home>
2025-08-19 19:14:48 +0500

    Model is trained

--- backend/account/views.py
+++ backend/account/views.py
@@ -5,7 +5,11 @@ from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
 
 
+
 class RegisterView(generics.CreateAPIView):
     queryset=User.objects.all()
     serializer_class= UserSerializer
     permission_classes=[AllowAny]
+
+
+
